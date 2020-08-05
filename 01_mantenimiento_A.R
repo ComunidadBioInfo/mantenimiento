d5a83dd353c7f0998bdaea080d24cf775c35f877
Joselyn Chavez <joselynchavezf@gmail.com>
2020-08-05 00:25:34 -0500

    add files

---
+++ 01_mantenimiento_A.R
@@ -0,0 +1,19 @@
+# Texto sin acentos
+
+# Revisa la version que tienes instalada actualmente de ggplot2
+# Pista: la función packageVersion() puede ser útil
+
+# Busca la pagina de este paquete en CRAN
+
+# Busca la lista de versiones anteriores
+
+# Instala una version anterior de ggplot2
+# Pista: la función devtools::install_version() puede ser util
+
+# Verifica que version tienes ahora?
+
+# La funcion old.packages() nos da la lista de paquetes que tienen disponible
+# una versión actualizada, usala y verifica que detecte el paquete ggplot2
+
+# Actualiza este paquete usando alguna de las funciones que mencionamos
+# previamente en la presentacion
